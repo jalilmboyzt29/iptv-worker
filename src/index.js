e84e3a697a1d380313db76ff964daa682dc57237
Jalil <dev@jll.my.id>
2026-06-24 18:51:45 +0900

    perbaikan Referer di headers

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { connect } from 'cloudflare:sockets';
 
 const HEADERS = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-  "Referer": "http://vzagut73megogo.xyz"
+  "Referer": "http://vzagut73.megogo.xyz"
 };
 
 export default {
